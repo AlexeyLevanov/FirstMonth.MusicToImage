53be30942dd2a3271aee3444d206b54ed1603e0c
Alexey Levanov <alexeylevanov27@gmail.com>
2021-08-10 22:17:40 +0300

    CHLNG - погнали!

---
+++ 1. MusicToImage/1. MusicToImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1. MusicToImage
+//
+//  Created by Alexey Levanov on 10.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
